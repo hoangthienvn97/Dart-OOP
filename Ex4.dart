e9ddd215004ae428d418f5a47f93ed77c291bc81
Nguyễn Hoàng Thiện <hoangthienvn97@gmail.com>
2019-12-25 13:41:08 +0700

    done ex-5

--- Ex4.dart
+++ Ex4.dart
@@ -1,13 +1,17 @@
 library faker.example;
 
 import 'dart:math';
-
 import 'package:faker/faker.dart';
 
 abstract class Animal {
+
   String name;
   void say() {
     print("Grr.....");
+  }
+
+  String getClass() {
+    return "Animal";
   }
 
   String getName() {
@@ -28,12 +32,22 @@ class Cat extends Animal {
   void say() {
     print("Moew. My name is ${name}");
   }
+
+  @override
+  String getClass() {
+    return "Cat";
+  }
 }
 
 class Dog extends Animal {
   Dog(String name) : super(name);
   void say() {
     print("Woofff. My name is ${name}");
+  }
+
+  @override
+  String getClass() {
+    return "Dog";    
   }
 }
 
@@ -55,8 +69,24 @@ List<Animal> generate(int n) {
 }
 
 main() {
-  var list = generate(10);
+  var list = generate(20);
+
+  var comparator = (Animal a, Animal b) {
+    if (a.getClass() == b.getClass()) {
+      return a.name.compareTo(b.name);
+    }
+
+    return b.getClass().compareTo(a.getClass());
+  };
+
+  list.sort(comparator);
+
   for (var item in list) {
-    item.say();
+    if (item is Cat) {
+      print("CAT");
+    } else if (item is Dog) {
+      print("DOG");
+    }
+    item.say(); 
   }
 }
